8be86612250908ca38a9ed7934c7c66dc2275b01
deepaknirala <deepakniralabgp@gmail.com>
2025-06-10 22:39:47 +0530

    Factorial of a number N

---
+++ 00_Basics/2_Basic_Recursion/06_FactorialofN.cpp
@@ -0,0 +1,18 @@
+//Problem Statement: Given a number N,  print its factorial.
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n){   
+   if(n == 0) //Base condition.
+       return 1; // 0! is defined as 1.
+
+   // Problem broken down into 2 parts and then combined.
+   return n * factorial(n-1);
+}
+
+int main(){
+  int n = 5;
+  cout<<factorial(n)<<endl;
+  return 0;
+}
